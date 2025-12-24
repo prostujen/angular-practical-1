551129612fc41a69f632068961f836dd7ba9df1f
prostujen <bobrdobrj69@gmail.com>
2025-12-24 17:20:08 +0200

    Completed task 3: Added data binding, ngFor, ngIf and dynamic styles

--- src/app/item-card/item-card.ts
+++ src/app/item-card/item-card.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core'; 
+import { CommonModule } from '@angular/common';
+import { Product } from '../shared/models/product.model'; 
 
 @Component({
   selector: 'app-item-card',
-  standalone: true, // <--- ДОДАЙТЕ ЦЕЙ РЯДОК
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './item-card.html',
-  styleUrl: './item-card.scss' // або styleUrl: './item-card.scss'
+  styleUrl: './item-card.scss'
 })
 export class ItemCardComponent {
+
+  @Input() item!: Product; 
 }
