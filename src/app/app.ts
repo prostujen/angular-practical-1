dcb8bdbc226f99143accfd3258c207072915bd2d
prostujen <bobrdobrj69@gmail.com>
2025-12-24 15:15:25 +0200

    Completed practical task 1: Layout and Interpolation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LayoutComponent } from './layout/layout'; 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, LayoutComponent], 
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('my-app');
+  title = 'my-app';
 }
---
+++ src/app/layout/layout.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layout',
+  templateUrl: './layout.html',
+  styleUrl: './layout.scss'
+})
+export class LayoutComponent {
+
+  appName = 'My Angular Project'; 
+}
